e4ef6f9454f3e1177d32a06f4fcc8dfacee832ad
zassamand <halilibrahimeris3@gmail.com>
2024-03-16 13:14:56 +0300

    Laser Prefab'i yapıldı

--- Assets/_game/Scripts/Laser/Laser.cs
+++ Assets/_game/Scripts/Laser/Laser.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    public GameObject rayOriginObject;
-
+    [SerializeField] private float _laserLenght;
     // Update is called once per frame
     void Update()
     {
-        if (rayOriginObject != null)
+        // Raycast için baþlangýç noktasý olarak belirlediðimiz objenin pozisyon ve rotasyonu üzerinden bir ýþýn oluþtur
+        Ray ray = new Ray(gameObject.transform.position, gameObject.transform.forward);
+
+        // Sonsuz mesafeye kadar ýþýn gönder
+        float rayDistance = _laserLenght;
+
+        // Iþýný görselleþtirme (isteðe baðlý)
+        Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.black);
+
+        // Raycast ile iþlem yap
+        if (Physics.Raycast(ray, out RaycastHit hit, rayDistance))
         {
-            // Raycast için baþlangýç noktasý olarak belirlediðimiz objenin pozisyon ve rotasyonu üzerinden bir ýþýn oluþtur
-            Ray ray = new Ray(rayOriginObject.transform.position, rayOriginObject.transform.forward);
-
-            // Sonsuz mesafeye kadar ýþýn gönder
-            float rayDistance = 10f;
-
-            // Iþýný görselleþtirme (isteðe baðlý)
-            Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.black);
-
-            // Raycast ile iþlem yap
-            if (Physics.Raycast(ray, out RaycastHit hit, rayDistance))
-            {
+            if (hit.collider.GetComponent<PlayerMovementController>() != null)
+                Debug.Log("Playera Çarptý");
+            else
                 Debug.Log("Iþýn bir nesneye çarptý: " + hit.collider.gameObject.name);
-            }
         }
     }
 }
